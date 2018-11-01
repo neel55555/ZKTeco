a7f4a86f1c119005a8912bce987e5bbb04bdeb62
neel55555 <neelion333@gmail.com>
2018-11-01 18:24:33 +0600

    01-11-2018

--- src/Controller/Api/Device.php
+++ src/Controller/Api/Device.php
@@ -6,43 +6,51 @@ use App\Controller\BaseController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 use App\Form\DeviceType;
 
-use App\Entity\Device as EntityDevice;
+use App\Entity\Devices as EntityDevices;
 
 
 class Device extends BaseController
 {
 
     /**
-     * @Route("/api/device")
-     * @Method("POST")
+     * @Route("/api/device", methods={"GET", "POST"})
      */
     public function addDevice(Request $request)
     {
         
+        $response = new Response();
+        $em = $this->getDoctrine()->getManager();
+
         $form = $this->createForm(DeviceType::class);
         $data = json_decode($request->getContent(), true);
         $form->submit($data);
 
-        if($form->isSubmitted() && $form->isValid()){
-            $devices = $form->getData();
-            $devices->setTimeLastLog(new \DateTime($data['timeLastLog']));
+        if($request->isMethod('post')){
 
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($devices);
-            $em->flush();
-            
-            $response = new Response(json_encode([]));
-            $response->setStatusCode(Response::HTTP_CREATED);
-            $response->headers->set('Access-Control-Allow-Origin', '*');
-            return $response;
+            if($form->isSubmitted() && $form->isValid()){
+                $devices = $form->getData();
+                $devices->setTimeLastLog(new \DateTime($data['timeLastLog']));
+
+                
+                $em->persist($devices);
+                $em->flush();
+                
+                $response->setContent(json_encode(["status"=>"Device Created"]));
+                $response->setStatusCode(Response::HTTP_CREATED);
+                $response->headers->set('Access-Control-Allow-Origin', '*');
+                return $response;
+            };
         };
 
-        
+        $devices = $em->getRepository(EntityDevices::class)->findAll();
 
+
+        $response->setContent(json_encode($devices));
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        return $response;
     }
 
 }
